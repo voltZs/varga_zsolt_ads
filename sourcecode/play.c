4175ac43052cb426f7e74b1744b143c5deccac50
Zsolt Varga <zs.zsolt.varga@gmail.com>
2019-03-07 22:26:48 +0000

    Making gameboard and winscore dynamic. Size of board as well as marks in a row required for a win should be customisable now

--- sourcecode/play.c
+++ sourcecode/play.c
@@ -10,10 +10,6 @@
 #define LEADERBOARD 3
 #define EXIT 0
 
-// hopefully make these 2 dynamic
-#define GB_SIZE 3
-#define WIN_SCORE 3
-
 typedef enum { false, true } bool;
 
 struct player{
@@ -24,20 +20,22 @@ struct player{
 
 struct turn{
     struct player * owner;
-    int positionX;
-    int positionY;
+    int row;
+    int column;
     struct turn * next;
 };
 
 void setupGame();
+void initBoard(int***, int);
+void traverse_board(int**, int);
 void push_turn(struct turn **, struct turn *);
 void traverse_turns(struct turn *);
 void flush_turns(struct turn **);
 struct turn * pop_turn(struct turn **);
-void playTurn(struct player**, struct turn**, struct turn**);
+void playTurn(struct player**, struct turn**, bool, int**, int);
 void makeMove();
-int checkGameState();
-void displayBoard();
+void checkGameState(int **, int, int, int *, struct player **);
+void displayBoard(int **, int);
 void printDivider(int);
 void printTop(int);
 void printLine(int, int*, int);
@@ -46,10 +44,8 @@ int readinput(char s[], int);
 int rowToInt(char);
 char getSign(int);
 
-int gamestate = 0;
-bool sessionstate = GAME;
 // 0 => NO INPUT, 1 => X, 2 => O
-int board[GB_SIZE][GB_SIZE]= {{0,0,0}, {0,0,0}, {0,0,0}};
+// int board[size][size]= {{0,0,0,0}, {0,0,0,0}, {0,0,0,0}, {0,0,0,0}};
 
 //other prompts: new players, end game, show leaderboard,
 
@@ -60,11 +56,20 @@ int board[GB_SIZE][GB_SIZE]= {{0,0,0}, {0,0,0}, {0,0,0}};
 /////////////////////////////////  MAIN  ///////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 int main(){
+    int gb_size = 4;
+    int ** board;
+    initBoard(&board, gb_size);
+    traverse_board(board, gb_size);
+
+    int winscore = 3;
+    int gamestate = 0;
+    int sessionstate = GAME;
     // both implemented as stacks
     struct turn * history;
     history = NULL;
     struct turn * undoneHistory;
     undoneHistory = NULL;
+
     struct player * playerOne;
     struct player * playerTwo;
 
@@ -76,10 +81,7 @@ int main(){
     playerTwo -> mark = MARK_O;
     playerTwo -> automated = false;
 
-    struct player ** currPlayer = &playerOne;
-
-
-    printf("Player 1 mark: %c", getSign(playerOne -> mark));
+    struct player ** currPlayer = &playerTwo;
 
     //setup game - ask for name, preferred mark, 1 vs 1 or 1 vs computer
     //
@@ -87,23 +89,33 @@ int main(){
     {
         if(sessionstate == GAME)
         {
-            while(!gamestate){
-                // different logic for both modes, duh
-                // ask about turn
-                displayBoard();
-                playTurn(currPlayer, &history, &undoneHistory);
-                // if statement prevents from currPlayer being switched if game won
-                if(!gamestate){
+            while(1){
+                displayBoard(board, gb_size);
+                checkGameState(board, gb_size, winscore, &gamestate, currPlayer);
+
+                if(gamestate)
+                {
+                    break;
+                } else {
                     if(*currPlayer == playerOne){
                         currPlayer = &playerTwo;
                     }else{
                         currPlayer = &playerOne;
                     }
                 }
+
+                playTurn(currPlayer, &history, (*currPlayer)->automated, board, gb_size);
             }
+
+            if(gamestate == GAME_WON){
+                printf("Player %d (%c) won!\n", (*currPlayer) -> mark, getSign((*currPlayer) -> mark));
+            } else if(gamestate == GAME_TIE){
+                printf("It's a tie!\n");
+            }
+
             // test/ demontration of using pop
             // struct turn * abcd = pop_turn(&history);
-            // printf("Popped turn: %c(%d,%d);;; ", getSign((abcd->owner)->mark), abcd-> positionX, abcd->positionY);
+            // printf("Popped turn: %c(%d,%d);;; ", getSign((abcd->owner)->mark), abcd-> row, abcd->column);
             // however when popping we would just add it straight to undoneHistory:
             // push(&undoneHistory, pop_turn(&history));
 
@@ -111,22 +123,16 @@ int main(){
             // flush_turns(&history);
             // traverse_turns(history);
 
-            displayBoard();
-            if(gamestate == GAME_WON){
-                printf("Player %d (%c) won!\n", (*currPlayer) -> mark, getSign((*currPlayer) -> mark));
-            } else if(gamestate == GAME_TIE){
-                printf("It's a tie!\n");
-            }
         }
 
-        if(sessionstate = LEADERBOARD)
+        if(sessionstate == LEADERBOARD)
         {
 
         }
 
-        if(sessionstate = PREFERENCES)
+        if(sessionstate == PREFERENCES)
         {
-            
+
         }
 
 
@@ -148,41 +154,45 @@ void setupGame()
 
 }
 
-void playTurn(struct player ** currPlayer, struct turn ** history, struct turn ** undoneHistory){
-    char row = 0;
-    int rowNumeral = 0;
+void playTurn(struct player ** currPlayer, struct turn ** history, bool automated, int** board, int size){
     int column = 0;
-    char input[3];
-    printf("Player %d (%c): Enter row followed by column\n", (*currPlayer) -> mark, getSign((*currPlayer) -> mark));
-    readinput(input, 3);
-    row = input[0];
-    rowNumeral = rowToInt(row);
-    column = atoi(&input[1]);
+    int row = 0;
 
-    if(rowNumeral <= 0 || rowNumeral > GB_SIZE || column <= 0 || column > GB_SIZE)
+    if(automated){
+        printf("Computer making a move.\n");
+        // LOGIC FOR PICKING ROW AND COLUMN AS COMPUTER
+    } else {
+        char input[3];
+        char rowChar = 0;
+        printf("Player %d (%c): Enter row followed by column\n", (*currPlayer) -> mark, getSign((*currPlayer) -> mark));
+        readinput(input, 3);
+        rowChar = input[0];
+        row = rowToInt(rowChar)-1;
+        column = atoi(&input[1])-1;
+    }
+
+    if(row < 0 || row > size || column < 0 || column > size)
     {
         printf("Not a valid input. Try again.\n");
-        playTurn(currPlayer, history, undoneHistory);
+        playTurn(currPlayer, history, automated, board, size);
         return;
     }
 
     // check if chosen tile is free
-    if(board[rowNumeral-1][column-1]  == 0)
+    if(board[row][column]  == 0)
     {
         struct turn * currTurn;
         currTurn = malloc(sizeof(struct turn));
         currTurn -> owner = (*currPlayer);
-        currTurn -> positionX = rowNumeral-1;
-        currTurn -> positionY = column-1;
+        currTurn -> row = row;
+        currTurn -> column = column;
         push_turn(history, currTurn);
-        board[rowNumeral-1][column-1] = (*currPlayer) -> mark;
+        board[row][column] = (*currPlayer) -> mark;
     }  else {
         printf("This tile is already taken. Make a different move.\n");
-        playTurn(currPlayer, history, undoneHistory);
+        playTurn(currPlayer, history, automated, board, size);
         return;
     }
-
-    gamestate = checkGameState();
     return;
 }
 
@@ -214,7 +224,7 @@ void traverse_turns(struct turn * history)
 {
     while(history != NULL)
     {
-        printf("%c: (%d,%d); ", getSign((history-> owner) -> mark),history -> positionX,  history -> positionY);
+        printf("%c: (%d,%d); ", getSign((history-> owner) -> mark),history -> row,  history -> column);
         history = history -> next;
     }
     printf("NULL\n");
@@ -229,6 +239,28 @@ void flush_turns(struct turn ** phistory)
     } else {
         free(*phistory);
         (*phistory) = NULL;
+    }
+}
+
+void initBoard(int *** board, int size){
+    // *board is a ** int after dereferencing
+    *board = malloc(sizeof(int *) * size);;
+    for(int row = 0; row<size; row++){
+        (*board)[row] = malloc(sizeof(int) * size);
+        for(int column = 0; column<size; column++){
+            (*board)[row][column] = 0;
+        }
+    }
+}
+
+void traverse_board(int ** board, int size){
+    for(int row = 0; row<size; row++)
+    {
+        for(int column = 0; column<size; column++){
+            printf("%d\t", board[row][column]);
+
+        }
+        printf("\n");
     }
 }
 
@@ -253,24 +285,28 @@ int rowToInt(char row){
     return numeric;
 }
 
-int checkGameState(){
+void checkGameState(int** board, int size, int winscore, int * gamestate, struct player ** player){
     int consecutive = 1;
     int current_sign;
     int previous_sign;
     int freeSpaces = 0;
 
+    bool automated = (*player) -> automated;
+
+
     // check horizontal - ALSO CHECKS IF GAME IS A TIE
-    for(int row = 0; row<GB_SIZE; row++){
+    for(int row = 0; row<size; row++){
         // reset values on new row
         previous_sign = 0;
         consecutive = 1;
-        for(int column = 0; column<GB_SIZE; column++){
+        for(int column = 0; column<size; column++){
             current_sign = board[row][column];
             if(current_sign == MARK_X || current_sign == MARK_O){
                 if(current_sign == previous_sign){
                     consecutive++;
-                    if(consecutive == WIN_SCORE){
-                        return GAME_WON;
+                    if(consecutive == winscore){
+                        *gamestate = GAME_WON;
+                        return;
                     }
                 } else {
                     consecutive = 1;
@@ -284,17 +320,18 @@ int checkGameState(){
     }
 
     // check vertical
-    for(int column = 0; column<GB_SIZE; column++){
+    for(int column = 0; column<size; column++){
         // reset values on new row
         previous_sign = 0;
         consecutive = 1;
-        for(int row = 0; row<GB_SIZE; row++){
+        for(int row = 0; row<size; row++){
             current_sign = board[row][column];
             if(current_sign == MARK_X || current_sign == MARK_O){
                 if(current_sign == previous_sign){
                     consecutive++;
-                    if(consecutive == WIN_SCORE){
-                        return GAME_WON;
+                    if(consecutive == winscore){
+                        *gamestate = GAME_WON;
+                        return;
                     }
                 } else {
                     consecutive = 1;
@@ -316,24 +353,24 @@ int checkGameState(){
     // direction 1 -> left to right direction 2 -> right to left (both top to bottom)
     for(int direction = 1; direction<3; direction++){
         increment = 1;
-        iterations = WIN_SCORE;
-        while(iterations >= WIN_SCORE){
+        iterations = winscore;
+        while(iterations >= winscore){
             previous_sign = 0;
             consecutive = 1;
 
-            if(iterations == GB_SIZE){
+            if(iterations == size){
                 increment *= -1;
             }
 
             if(direction == 1){
                 // while rising
                 if(increment == 1){
-                    column = GB_SIZE-(iterations-1)-1;
+                    column = size-(iterations-1)-1;
                     row = 0;
                 // while decreasing
                 } else if(increment == -1){
                     column = 0;
-                    row = GB_SIZE-(iterations-1)-1;
+                    row = size-(iterations-1)-1;
                 }
             } else if(direction ==2){
                 // while rising
@@ -342,8 +379,8 @@ int checkGameState(){
                     row = 0;
                 // while decreasing
                 } else if(increment == -1){
-                    column = GB_SIZE-1;
-                    row = GB_SIZE-iterations;
+                    column = size-1;
+                    row = size-iterations;
                 }
             }
 
@@ -352,8 +389,9 @@ int checkGameState(){
                 if(current_sign == MARK_X || current_sign == MARK_O){
                     if(current_sign == previous_sign){
                         consecutive++;
-                        if(consecutive == WIN_SCORE){
-                            return GAME_WON;
+                        if(consecutive == winscore){
+                            *gamestate = GAME_WON;
+                            return;
                         }
                     } else {
                         consecutive = 1;
@@ -377,42 +415,44 @@ int checkGameState(){
 
 
     if(!freeSpaces){
-        return GAME_TIE;
+        *gamestate = GAME_TIE;
+        return;
     }
 
-    return 0;
+    return;
 }
 
-void displayBoard(){
-    int width = (sizeof(board[0]))/(sizeof(board[0][0]));
-    int height = (sizeof(board))/(sizeof(board[0]));
+void displayBoard(int ** board, int size)
+{
     printf("\n");
 
     char letter = 'A';
-    printTop(width);
+    printTop(size);
 
-    for(int i = 0; i< height; i++){
-        printRow(letter+i, width, board[i]);
+    for(int i = 0; i< size; i++){
+        printRow(letter+i, size, board[i]);
     }
-    printDivider(width);
+    printDivider(size);
     printf("\n");
 }
 
-void printTop(int width){
+void printTop(int size)
+{
     printf("\t");
     printf("    ");
-    for(int i=0; i<width; i++){
+    for(int i=0; i<size; i++){
         printf("%d", i+1);
         printf("       ");
     }
     printf("\n");
 }
 
-void printRow(char letter, int width, int* values){
-    printDivider(width);
+void printRow(char letter, int size, int* values)
+{
+    printDivider(size);
 
     printf("\t");
-    printLine(width, values, 1);
+    printLine(size, values, 1);
 
     for(int i=0; i<8; i++){
         if(i==6){
@@ -421,14 +461,15 @@ void printRow(char letter, int width, int* values){
             printf(" ");
         }
     }
-    printLine(width, values, 2);
+    printLine(size, values, 2);
 
     printf("\t");
-    printLine(width, values, 3);
+    printLine(size, values, 3);
 }
 
-void printLine(int width, int* values, int vertical){
-    for(int i=0; i<(width); i++){
+void printLine(int size, int* values, int vertical)
+{
+    for(int i=0; i<(size); i++){
         printf("|");
         //EMPTY
         if(values[i]==0){
@@ -468,9 +509,10 @@ void printLine(int width, int* values, int vertical){
     printf("|\n");
 }
 
-void printDivider(int width){
+void printDivider(int size)
+{
     printf("\t");
-    for(int i=0; i<((width*7)+(width+1)); i++){
+    for(int i=0; i<((size*7)+(size+1)); i++){
         printf("-");
     }
     printf("\n");
